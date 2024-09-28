329c6a10c3ff53d1de82207430df4b98a742fd4f
ayberkozcan <ozcanayberk249@gmail.com>
2024-09-28 17:01:51 +0300

    Update To Do App Project
    Showing tasks and deleting task function

--- To Do App/to_do_app.py
+++ To Do App/to_do_app.py
@@ -16,8 +16,11 @@ class ToDoApp(ctk.CTk):
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         self.theme_icon_path = os.path.join(BASE_DIR, "icons/theme_icon.png")
         self.add_task_icon_path = os.path.join(BASE_DIR, "icons/add_task_icon.png")
+        self.trash_can_icon_path = os.path.join(BASE_DIR, "icons/trash_can_icon.png")
+        self.confirmation_icon_path = os.path.join(BASE_DIR, "icons/confirmation_icon.png")
 
         self.widgets()
+        self.task_list()
 
     def switch_theme(self):
 
@@ -40,6 +43,7 @@ class ToDoApp(ctk.CTk):
             with open("tasks.txt", "a") as file:
                 file.write(f"{task}\n")
             self.result.configure(text="Task Saved!", text_color="green")
+            self.task_list()
         else:
             self.result.configure(text="No task entered!", text_color="red")
 
@@ -47,10 +51,73 @@ class ToDoApp(ctk.CTk):
 
         self.add_entry.pack_forget()
         self.add_task_button.pack_forget()
-        self.add_entry_button.pack(pady=10)
+        self.add_entry_button.pack(side="bottom", pady=10)
 
     def clear_message(self):
         self.result.configure(text="")
+
+    def task_list(self):
+        if hasattr(self, 'records_frame'):
+            self.records_frame.destroy()
+
+        try:
+            with open("tasks.txt", "r") as file:
+                tasks = file.readlines()
+
+            if tasks:
+                self.task_labels = []
+
+                self.records_frame = ctk.CTkFrame(self)
+                self.records_frame.pack(pady=10, fill="x")
+
+                for i, task in enumerate(tasks, start=1):
+                    formatted_task = task.strip()
+
+                    task_label = ctk.CTkLabel(master=self.records_frame, text=formatted_task, font=("Helvetica", 20))
+                    task_label.grid(row=i, column=0, padx=10, pady=5, sticky='w')
+                    self.task_labels.append(task_label)
+
+                    completed_button = ctk.CTkButton(master=self.records_frame, image=self.confirmation_icon, text="", command=lambda idx=i: self.complete_task(idx), width=30)
+                    completed_button.grid(row=i, column=1, padx=10, pady=5)
+
+                    delete_button = ctk.CTkButton(master=self.records_frame, image=self.trash_can_icon, text="", command=lambda idx=i: self.delete_task(idx), fg_color="red", width=30)
+                    delete_button.grid(row=i, column=2, padx=10, pady=5)
+
+            else:
+                self.result.configure(text="No Tasks Available...", text_color="gray")
+
+        except FileNotFoundError:
+            self.result.configure(text="No Tasks Available...", text_color="gray")
+
+    def complete_task(self, task_index):
+        try:
+            with open("tasks.txt", "r") as file:
+                lines = file.readlines()
+
+            if 0 <= task_index - 1 < len(lines):
+                self.task_labels[task_index - 1].configure(font=("Helvetica", 20, "overstrike"))
+
+                self.task_list()
+
+        except FileNotFoundError:
+            self.result.configure(text="No Tasks Available...", text_color="gray")
+
+
+    def delete_task(self, task_index):
+        try:
+            with open("tasks.txt", "r") as file:
+                lines = file.readlines()
+
+            if 0 <= task_index - 1 < len(lines):
+                del lines[task_index - 1]
+
+                with open("tasks.txt", "w") as file:
+                    file.writelines(lines)
+
+            self.task_list()
+
+        except FileNotFoundError:
+            self.result.configure(text="No Tasks Available...", text_color="gray")
 
     def widgets(self):
         self.header_homepage = ctk.CTkLabel(
@@ -93,7 +160,7 @@ class ToDoApp(ctk.CTk):
             width=40,
             height=40
         )
-        self.add_entry_button.pack(pady=10)
+        self.add_entry_button.pack(side="bottom", pady=40)
 
         self.add_task_button = ctk.CTkButton(
             self,
@@ -109,6 +176,12 @@ class ToDoApp(ctk.CTk):
             font=("Helvetica", 28),
         )
         self.result.pack(pady=50)
+
+        self.trash_can_icon = PhotoImage(file=self.trash_can_icon_path)
+        self.trash_can_icon = self.trash_can_icon.subsample(20, 20)
+
+        self.confirmation_icon = PhotoImage(file=self.confirmation_icon_path)
+        self.confirmation_icon = self.confirmation_icon.subsample(20, 20)
 
 if __name__ == "__main__":
     app = ToDoApp()
