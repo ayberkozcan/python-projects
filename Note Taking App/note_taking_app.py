5caf648ebb114c5aebc01b25d6d2165d666e240e
ayberkozcan <ozcanayberk249@gmail.com>
2024-10-15 14:27:17 +0300

    Update the "Note Taking App" Project
    Accessing notes with password and fixing the font bug.

--- Note Taking App/note_taking_app.py
+++ Note Taking App/note_taking_app.py
@@ -20,6 +20,8 @@ class NoteTakingApp(ctk.CTk):
         self.password = ""
         self.tag_combobox = ""
 
+        self.access_note = False
+
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         self.theme_icon_path = os.path.join(BASE_DIR, "icons/theme_icon.png")
         self.edit_icon_path = os.path.join(BASE_DIR, "icons/edit_icon.png")
@@ -227,7 +229,7 @@ class NoteTakingApp(ctk.CTk):
             self.password = note_password_control
             self.set_password_result_label.configure(text="Password saved!", text_color="green")
             self.set_password_result_label.after(3000, self.password_window.destroy)
-            
+
     def customize_text(self):
         window = ctk.CTkToplevel(self)
         window.title("Customize")
@@ -385,46 +387,91 @@ class NoteTakingApp(ctk.CTk):
                     note_password = lines[4].replace("Password: ", "").strip()
                     note_tag = lines[5].replace("Tag: ", "").strip()
 
+                    self.font = note_font
+                    self.font_size = note_font_size
+
                     self.password = note_password
-                    #self.tag_combobox = note_tag
-                    self.tag_combobox.set(note_tag)
-                    
-                    self.note_entry.configure(font=(note_font, int(note_font_size)))
 
-                    note_title = lines[0].replace("Title: ", "").strip()
-                    note_content = "".join(lines[7:])
+                    if self.password != "":  # If password exists
+                        self.password_control_window = ctk.CTkToplevel(self)
+                        self.password_control_window.title("Password")
+                        self.password_control_window.geometry("250x250")
 
-                self.title_note_entry.delete(0, END)
-                self.title_note_entry.insert(0, note_title)
+                        set_password_control_label = ctk.CTkLabel(
+                            self.password_control_window,
+                            text="Password"
+                        )
+                        set_password_control_label.grid(row=0, column=1, padx=10, pady=10)
 
-                self.note_entry.delete("1.0", END)
-                self.note_entry.insert("1.0", note_content)
+                        self.set_password_control_entry = ctk.CTkEntry(
+                            self.password_control_window,
+                            placeholder_text="...",
+                            width=200,
+                            height=30,
+                            border_width=1,
+                            corner_radius=10
+                        )
+                        self.set_password_control_entry.grid(row=1, column=1, padx=10, pady=10)
 
-            self.save_note_button.configure(
-                text="Update Note",
-                command=lambda: self.update_existing_note(file_name)
-            )
+                        self.set_password_control_result_label = ctk.CTkLabel(
+                            self.password_control_window,
+                            text="",
+                            font=("Helvetica", 14)
+                        )
+                        self.set_password_control_result_label.grid(row=2, column=1, padx=10, pady=10)
 
-            self.set_password_button.configure(
-                text="Change Password",
-                command=self.set_password_window
-            )
+                        self.set_password_control_confirm_button = ctk.CTkButton(
+                            self.password_control_window,
+                            text="Confirm",
+                            command=lambda: self.password_control(note_font, note_font_size, note_tag, lines, file_name)
+                        )
+                        self.set_password_control_confirm_button.grid(row=3, column=1, padx=10, pady=10)
+                    else: # If password doesn't exist
+                        self.load_note_content(note_font, note_font_size, note_tag, lines, file_name)
 
-            self.warning_label.place(x=50, y=620)
-            self.warning_label.after(4000, self.warning_clear_message)
+    def password_control(self, note_font, note_font_size, note_tag, lines, file_name):
+        if self.set_password_control_entry.get() == self.password:
+            self.set_password_control_result_label.configure(text="Password correct!", text_color="green")
+            self.access_note = True
+            self.password_control_window.after(2000, self.password_control_window.destroy)
 
-            self.result_label.configure(text="Note Loaded", text_color="yellow")
+            self.load_note_content(note_font, note_font_size, note_tag, lines, file_name)
         else:
-            self.result_label.configure(text="Note not found!", text_color="red")
+            self.set_password_control_result_label.configure(text="Try Again!", text_color="red")
+            self.access_note = False
+            self.set_password_control_entry.delete(0, END)
 
-        self.after(2000, self.clear_message)    
+    def load_note_content(self, note_font, note_font_size, note_tag, lines, file_name):
+        self.tag_combobox.set(note_tag)
+        self.note_entry.configure(font=(note_font, int(note_font_size)))
+
+        note_title = lines[0].replace("Title: ", "").strip()
+        note_content = "".join(lines[7:])
+
+        self.title_note_entry.delete(0, END)
+        self.title_note_entry.insert(0, note_title)
+
+        self.note_entry.delete("1.0", END)
+        self.note_entry.insert("1.0", note_content)
+
+        self.save_note_button.configure(
+            text="Update Note",
+            command=lambda: self.update_existing_note(file_name)
+        )
+
+        self.set_password_button.configure(
+            text="Change Password",
+            command=self.set_password_window
+        )
+
+        self.warning_label.place(x=50, y=620)
+        self.warning_label.after(4000, self.warning_clear_message)
+
+        self.result_label.configure(text="Note Loaded", text_color="yellow")
 
     def update_existing_note(self, file_name):
         note_content = self.note_entry.get("1.0", "end-1c")
         note_title = self.title_note_entry.get()
-
-        note_font = self.font
-        note_font_size = self.font_size
 
         note_tag = self.tag_combobox.get()
         #note_tag = self.tag_combobox
@@ -439,12 +486,14 @@ class NoteTakingApp(ctk.CTk):
         file_path = os.path.join(notes_directory, file_name)
 
         note_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        note_data = f"Title: {note_title}\nDate: {note_date}\nFont: {note_font}\nFont Size: {note_font_size}\nPassword: {note_password}\nTag: {note_tag}\n\n{note_content}"
+        note_data = f"Title: {note_title}\nDate: {note_date}\nFont: {self.font}\nFont Size: {self.font_size}\nPassword: {note_password}\nTag: {note_tag}\n\n{note_content}"
 
         with open(file_path, "w") as file:
             file.write(note_data)
 
         self.result_label.configure(text="Note Updated!", text_color="green")
+
+        self.access_note = False
 
         self.title_note_entry.delete(0, END)
         self.note_entry.delete("1.0", END)
@@ -488,6 +537,8 @@ class NoteTakingApp(ctk.CTk):
         self.set_password_result_label.configure(text="")
 
     def clear_screen(self):
+        self.access_note = False
+
         self.title_note_entry.delete(0, END)
         self.note_entry.delete("1.0", END)
 
